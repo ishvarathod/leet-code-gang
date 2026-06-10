3f9b797284fc5981453b3a6adb97fea396c8e4b3
ishvarathod <ishvarathod@gmail.com>
2026-06-11 00:32:24 +0930

    semi working

---
+++ LC_03.py
@@ -0,0 +1,30 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    max_string = ""
+    max_count = 0
+    for char in s:
+        if char in max_string:
+            max_string = ""
+            max_string = max_string + char
+            print("max_string of in str =", repr(max_string))
+        else:
+            max_string = max_string + char
+            print("max_string if not in str =", repr(max_string))
+
+            
+            if len(max_string) > max_count:            
+                max_count = len(max_string)
+
+    return max_count
+
+
+
+
+def main():
+    # s = "aab"
+    s = "dvdf"
+    print(lengthOfLongestSubstring(s))
+
+
+
+if __name__ == "__main__":
+    main()
