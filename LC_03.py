76527217141a689599faf4aa66ec8d400d5e1b6f
ishvarathod <ishvarathod@gmail.com>
2026-06-11 01:10:50 +0930

    on line 8, was indexing the wrong string lol, easy fix and we got there

--- LC_03.py
+++ LC_03.py
@@ -3,18 +3,11 @@ def lengthOfLongestSubstring(s: str) -> int:
     max_count = 0
     for char in s:
         if char in max_string:
-            index = s.find(char)
-            max_string = max_string[index + 1:]
             max_string = max_string + char
-
-            print("max_string if in str and go back=", repr(max_string))
-
-        
-            print("max_string if in str =", repr(max_string))
-
+            index = max_string.find(char)
+            max_string = max_string[index+1:]
         else:
             max_string = max_string + char
-            print("max_string if not in str =", repr(max_string))
 
             
             if len(max_string) > max_count:            
@@ -27,7 +20,10 @@ def lengthOfLongestSubstring(s: str) -> int:
 
 def main():
     # s = "aab"
-    s = "dvdf"
+    # s = "dvdf"
+    s ="aabaab!bb"
+
+
     print(lengthOfLongestSubstring(s))
 
 
