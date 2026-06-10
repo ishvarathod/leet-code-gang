a31509cc7c86240bac97b4ea5ab0f7163afa8c6e
ishvarathod <ishvarathod@gmail.com>
2026-06-11 00:39:41 +0930

    i think i got it

--- LC_03.py
+++ LC_03.py
@@ -3,9 +3,15 @@ def lengthOfLongestSubstring(s: str) -> int:
     max_count = 0
     for char in s:
         if char in max_string:
-            max_string = ""
+            index = s.find(char)
+            max_string = max_string[index + 1:]
             max_string = max_string + char
-            print("max_string of in str =", repr(max_string))
+
+            print("max_string if in str and go back=", repr(max_string))
+
+        
+            print("max_string if in str =", repr(max_string))
+
         else:
             max_string = max_string + char
             print("max_string if not in str =", repr(max_string))
